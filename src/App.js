299da0e72efde449aecf85740ee893434c162954
CristianGardosi <cristian.gardosi97@gmail.com>
2023-03-15 17:14:56 +0100

    Push

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import Product from "./Product";
 
+// Oggetto che popola le mie props definite in Product.js
+const productData = {
+  name: "Nome prodotto",
+  image: "https://media.costadelmar.com/cms/resource/image/88002/landscape_ratio1440x480/2880/960/7be350e6699103e01032099cee163365/5288D6DE764E220D02391D72D4706735/hpbanner-corbinapro.jpg"
+}
+
 function App() {
   return (
     <div className="App">
-      <Product></Product>
+      {/*Inserisco componente e props*/} 
+      {/*OPPURE POSSO USARE SPREAD OPERATOR {...productData} 
+      che se c'è corrispondenza tra props nel componente e chiavi nell'oggetto sostituisce la logica di riscrittura che vediamo qua*/}
+      <Product name={productData.name} image={productData.image}></Product>
     </div>
   );
 }
--- src/Product.js
+++ src/Product.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
-const Product = () => {
-  // Qua posso definire le mie variabili
-  const image = "https://media.costadelmar.com/cms/resource/image/88002/landscape_ratio1440x480/2880/960/7be350e6699103e01032099cee163365/5288D6DE764E220D02391D72D4706735/hpbanner-corbinapro.jpg";
+//Nelle props del componente definisco gli elementi dinamici (props è un oggetto)
+// Destrutturazione oggetto props per rendere il codice più chiaro e manutenibile
+const Product = ({name, image}) => {
   return (
     // React.Fragment = Wrapper che sostituisce <div> o altri tag e che non viene inserito nell'html
     <React.Fragment>
         <div className="card">
-            <img className="card-img" src={`${image}`} alt={`${image}`}></img> {/*oppure semplicmente src={image}*/}
+            <h2>{name}</h2>
+          <img className="card-img" src={image} alt={`${image}`}></img> {/*Entrambe le sitassi sono possibili*/}
         </div>
     </React.Fragment>
   )
